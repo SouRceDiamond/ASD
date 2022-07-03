8a5f710c2ce8be995bfcedeb26a52103feb61f86
SouRceDiamond <108604875+SouRceDiamond@users.noreply.github.com>
2022-07-03 17:29:04 +0200

    Update gcast.py

--- plugins/gcast.py
+++ plugins/gcast.py
@@ -12,9 +12,9 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("**`هاد حمار وش تبي اهين بي ...`**")
+        wtf = await message.reply("**`دا عيل حلنجيصه سيبك منو...`**")
         if not message.reply_to_message:
-            await wtf.edit("**صار لك جرجف انجب وزا تندك بسيادك اكتلك وزرب على كبرك 🖐💩 ...**")
+            await wtf.edit("**ازاي ههينو وهو معندوش كلامه اصلا 🖐...**")
             return
         lmao = message.reply_to_message.text
         async for dialog in aditya.iter_dialogs():
